3b042782b0829e24183b560d7491ea21bc1bf973
Shane Pham <sambidao88@gmail.com>
2024-03-06 09:44:54 +0000

    Blog Day 1

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const expressLayout = require('express-ejs-layouts')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// Router
+app.use('/', require('./server/routes/main'))
+
+
+// Listening Port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
